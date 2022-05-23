ade18416df35f408e02bf96ca6f79b6a0b126e8d
gsierra22 <giancarlosierra2011@hotmail.com>
2022-05-22 23:58:46 -0500

    readme work

--- passport-config.js
+++ passport-config.js
@@ -4,10 +4,13 @@ const LocalStrategy = require('passport-local').Strategy
 
 function initialize(passport){
     const authenticateUser= (email, password, done) =>{
-
+        const user = getUserByEmail(email)
+        if (user == null){
+            return done(null, false, {message: 'No email found'})
+        }
     }
     passport.use(new LocalStrategy({usernameField: 'email'}),
     authenticateUser)
     passport.serializeUser((user, done)=>{ })
-    passport.deserializeUser((user, done)=>{ })
+    passport.deserializeUser((id, done)=>{ })
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
 ## Authentication Test App
-Hello! I am making this to practice making an authentication function for apps
+Hello! I am making this to practice making an authentication function for apps. This is currently a practice run that I will try to incorporate to later projects.
