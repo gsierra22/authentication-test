2e4f3c89dc9c93d7af74ec4631b3a82ec140e72e
gsierra22 <giancarlosierra2011@hotmail.com>
2022-05-26 11:46:14 -0500

    unathenticated check

--- server.js
+++ server.js
@@ -33,21 +33,21 @@ app.get('/', checkAuthenticated, (req, res)=>{
     res.render('index.ejs', {name: req.user.name})
 })
 
-app.get('/login',(req, res)=>{
+app.get('/login', checkNotAuthenticated,(req, res)=>{
     res.render('login.ejs')
 })
 
-app.post('/login', passport.authenticate('local',{
+app.post('/login', checkNotAuthenticated, passport.authenticate('local',{
     successRedirect:'/',
     failureRedirect: '/login',
     failureFlash: true
 }))
 
-app.get('/register',(req, res)=>{
+app.get('/register', checkNotAuthenticated,(req, res)=>{
     res.render('register.ejs')
 })
 
-app.post('/register', async (req, res)=>{
+app.post('/register', checkNotAuthenticated, async (req, res)=>{
     try{
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         users.push({
@@ -67,6 +67,13 @@ if (req.isAuthenticated()){
     return next()
 }
 res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+       return res.redirect('/')
+    }
+    next()
 }
 
 app.listen(3000)
