95eb2181b80fe9f9f5ab8effb7103b265ab37bcb
gsierra22 <giancarlosierra2011@hotmail.com>
2022-05-26 11:06:11 -0500

    config finished

--- passport-config.js
+++ passport-config.js
@@ -1,16 +1,30 @@
 const { authenticate } = require('passport/lib')
+const bcrypt = require('bcrypt')
 
 const LocalStrategy = require('passport-local').Strategy
 
-function initialize(passport){
+function initialize(passport, getUserByEmail){
     const authenticateUser= (email, password, done) =>{
         const user = getUserByEmail(email)
         if (user == null){
             return done(null, false, {message: 'No email found'})
         }
+
+        try {
+            if (await bcrypt.compare(password, user.password)){
+              return done (null, user)  
+            } else {
+                return done (null, false, {message: 'Incorrect Password'})
+            }
+        } catch (e){
+            return done (e)
+        }
     }
+    
     passport.use(new LocalStrategy({usernameField: 'email'}),
     authenticateUser)
     passport.serializeUser((user, done)=>{ })
     passport.deserializeUser((id, done)=>{ })
 }
+
+module.exports = initialize
